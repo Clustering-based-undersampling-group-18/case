24cf59577cb8c41d2de20fa0a9568e96285793ef
timvdt <timvdtoorn@live.nl>
2021-01-11 14:46:02 +0100

    update

--- CART.py
+++ CART.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
 # Importing data
+frame_2019 = pd.read_csv("data/data_2019.csv")  # 2110338
+frame_2020 = pd.read_csv("data/data_2020.csv")  # 2645037
+frame = pd.concat([frame_2019, frame_2020], ignore_index=True)  # 4755375
+
+# Splitting data
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
