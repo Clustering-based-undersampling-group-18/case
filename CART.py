595c5f6b9343f40cc619691a1ddf0c1fc735c37f
timvdt <timvdtoorn@live.nl>
2021-01-11 15:31:05 +0100

    Update CART.py

--- CART.py
+++ CART.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import DecisionTreeRegressor, export_graphviz
 from sklearn.model_selection import train_test_split
+from io import StringIO
+from IPython.display import Image
+import pydotplus
 
 # Importing data
 frame_2019 = pd.read_csv("data/data_2019.csv")  # 2110338
@@ -10,4 +13,19 @@ frame_2020 = pd.read_csv("data/data_2020.csv")  # 2645037
 frame = pd.concat([frame_2019, frame_2020], ignore_index=True)  # 4755375
 
 # Splitting data
-# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
+X = frame[['totalPrice', 'quantityOrdered']]
+Y = frame['noCancellation']
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
+
+# CART
+model = DecisionTreeRegressor(max_leaf_nodes=20)
+rt = model.fit(X_train, Y_train)
+DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None, max_leaf_nodes=20, min_impurity_decrease=0.0,
+                      min_impurity_split=None, min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0,
+                      random_state=None, splitter='best')
+dot_data = StringIO()
+export_graphviz(rt, out_file=dot_data)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.create_png())
+
+
