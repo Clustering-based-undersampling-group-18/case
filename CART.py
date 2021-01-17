5d80699bb59f00d8709bc532ab02172bb4138e6d
timvdt <timvdtoorn@live.nl>
2021-01-17 10:54:59 +0100

    Update CART.py

--- CART.py
+++ CART.py
@@ -13,16 +13,15 @@ frame_2020 = pd.read_csv("data/data_2020.csv")  # 2645037
 frame = pd.concat([frame_2019, frame_2020], ignore_index=True)  # 4755375
 
 # Splitting data
-X = frame[['totalPrice', 'quantityOrdered']]
+X = frame[['totalPrice', 'quantityOrdered', 'cntDistinctCaseIds']]
 Y = frame['noCancellation']
+X[np.isnan(X)] = 0
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
 
 # CART
 model = DecisionTreeRegressor(max_leaf_nodes=20)
+# one_hot_data = pd.get_dummies(frame[['A', 'B', 'C']], drop_first=True)
 rt = model.fit(X_train, Y_train)
-DecisionTreeRegressor(criterion='mse', max_depth=None, max_features=None, max_leaf_nodes=20, min_impurity_decrease=0.0,
-                      min_impurity_split=None, min_samples_leaf=1, min_samples_split=2, min_weight_fraction_leaf=0.0,
-                      random_state=None, splitter='best')
 dot_data = StringIO()
 export_graphviz(rt, out_file=dot_data)
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
