1ce76098785419c0ba6d27e82a0b989a3596fb93
timvdt <timvdtoorn@live.nl>
2021-01-25 13:37:21 +0100

    update

--- RandomForest.py
+++ RandomForest.py
@@ -8,23 +8,21 @@ import pydot
 import random
 
 # Importing data
-random.seed(1234)
+# random.seed(1234)
 frame_2019 = pd.read_csv("data/data_2019.csv")  # 2110338
 frame_2020 = pd.read_csv("data/data_2020.csv")  # 2645037
 frame = pd.concat([frame_2019, frame_2020], ignore_index=True)  # 4755375
 
 # Splitting data
-X = frame[['totalPrice', 'quantityOrdered', 'countryCode']]
+X = frame[['totalPrice', 'quantityOrdered', 'sellerId', 'countryCode']]
 X = pd.get_dummies(X)
 features = list(X.columns)
 X = X.to_numpy()
 # X[np.isnan(X)] = 0
-Y = frame['noReturn']
+Y = frame['noReturn', 'noCancellation']
 Y = Y.to_numpy()
-train_X, test_X, train_Y, test_Y = train_test_split(X, Y, test_size=0.3, random_state=1234)
-index = np.random.choice(np.arange(len(train_X)), 10000, replace=False)
-grid_X = train_X[index]
-grid_Y = train_Y[index]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, random_state=1234)
 
 
 # Hyperparameter sets
@@ -40,15 +38,15 @@ hyperparam = {
 # Grid search for the hyperparameters
 RF = RandomForestClassifier()
 grid_search = GridSearchCV(estimator=RF, param_grid=hyperparam, cv=3, n_jobs=-1, verbose=2)
-grid_search.fit(grid_X, grid_Y)
+grid_search.fit(X_val, Y_val)
 
 # Prints the best hyperparameters
 print("Best hyperparameter values:", grid_search.best_params_)
 
 # Predicts the test set using the best model from the grid search
 RF = RandomForestClassifier(**grid_search.best_params_)
-RF.fit(train_X, train_Y)
-score = RF.score(test_X, test_Y)
+RF.fit(X_train, Y_train)
+score = RF.score(X_test, Y_test)
 
 # Prints the accuracy of the prediction
 print("RF prediction accuracy:", score)
