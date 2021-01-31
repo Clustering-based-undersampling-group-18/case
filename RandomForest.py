678c2de689165f9209c451310fd380215d45ec1b
timvdt <timvdtoorn@live.nl>
2021-01-31 13:43:29 +0100

    Update RandomForest.py

--- RandomForest.py
+++ RandomForest.py
@@ -4,15 +4,14 @@ from scipy import stats
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.tree import export_graphviz
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GridSearchCV
 import pydot
 import random
 
 # Importing data
 random.seed(1234)
-frame_2019 = pd.read_csv("data/data_2019.csv")  # 2110338
-frame_2020 = pd.read_csv("data/data_2020.csv")  # 2645037
-frame = pd.concat([frame_2019, frame_2020], ignore_index=True)  # 4755375
+frame = pd.read_csv("data/frame.csv")
 
 # Splitting data
 X = frame[['totalPrice', 'quantityOrdered', 'sellerId', 'countryCode', 'productGroup']]
@@ -21,7 +20,6 @@ features = list(X.columns)
 X = X.to_numpy()
 Y = frame[['noCancellation', 'onTimeDelivery', 'noReturn', 'noCase']]
 Y = Y.to_numpy()
-Y[np.isnan(Y)] = 0.5
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=1234)
 X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size=0.2, random_state=1234)
 
@@ -36,14 +34,15 @@ hyper_param = {'n_estimators': stats.randint(150, 1000),
 
 # Grid search for the hyperparameters
 RF = XGBClassifier()
-grid_search = GridSearchCV(estimator=RF, param_grid=hyper_param, cv=3, n_jobs=-1, verbose=2)
-grid_search.fit(X_val, Y_val)
+random_search = RandomizedSearchCV(RF, param_distributions=hyper_param, cv=5, n_iter=5, scoring='roc_auc',
+                                   error_score=0, verbose=3, n_jobs=-1)
+random_search.fit(X_val, Y_val)
 
 # Prints the best hyperparameters
-print("Best hyperparameter values:", grid_search.best_params_)
+print("Best hyperparameter values:", random_search.best_params_)
 
 # Predicts the test set using the best model from the grid search
-RF = XGBClassifier(**grid_search.best_params_)
+RF = XGBClassifier(**random_search.best_params_)
 RF.fit(X_train, Y_train)
 score = RF.score(X_test, Y_test)
 
