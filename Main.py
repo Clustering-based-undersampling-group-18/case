0279174acd7c7883b6ca17d9f258ec9198309db2
timvdt <timvdtoorn@live.nl>
2021-02-11 14:25:40 +0100

    update

--- Main.py
+++ Main.py
@@ -3,25 +3,33 @@ from XGBoost import RandomForest
 
 # Importing data
 X_train = pd.read_csv("data/train_test_frames/final_train_x.csv")
-X_train = X_train.drop(columns={'index', 'sellerId', 'orderDate', 'Unnamed: 0'})
-X_train.to_numpy()
+X_train = X_train.drop(columns={'sellerId', 'orderDate', 'Unnamed: 0'})
+X_train = X_train.iloc[:, 1:]
+X_train = X_train.to_numpy()
 X_test = pd.read_csv("data/train_test_frames/final_test_x.csv")
-X_test = X_test.drop(columns={'index', 'sellerId', 'orderDate', 'Unnamed: 0'})
-X_test.to_numpy()
-Y_train = pd.read_csv("data/train_test_frames/final_train_y.csv").to_numpy()
-Y_test = pd.read_csv("data/train_test_frames/final_test_y.csv").to_numpy()
-
+X_test = X_test.drop(columns={'sellerId', 'orderDate', 'Unnamed: 0'})
+X_test = X_test.iloc[:, 1:]
+X_test = X_test.to_numpy()
+Y_train = pd.read_csv("data/train_test_frames/final_train_y.csv")
+Y_train = Y_train.drop(columns={'Unnamed: 0'})
+Y_train = Y_train.iloc[:, 1:]
+Y_train = Y_train.to_numpy()
+Y_test = pd.read_csv("data/train_test_frames/final_test_y.csv")
+Y_test = Y_test.drop(columns={'Unnamed: 0'})
+Y_test = Y_test.iloc[:, 1:]
+dep_vars = Y_test.columns
+Y_test = Y_test.to_numpy()
 
 # For loop over all dependent variables
-for (depend_train, depend_test) in (Y_train.T, Y_test.T):
-    depend_train = depend_train.T
-    depend_test = depend_test.T
-    print(depend_train.dtype.names[0])
+for i in range(0, 4):
+    criteria = dep_vars[i]
+    depend_train = Y_train[:, i]
+    depend_test = Y_test[:, i]
 
     # Predicting dependent variable with XGBoost Random Forest
-    RF = RandomForest(X_train, X_test, depend_train, depend_test)
-    print("XGB best parameters for {0}: ", RF.best_param).format(depend_train.dtype.names[0])
-    print("XGB prediction accuracy for {0}: ", RF.score).format(depend_train.dtype.names[0])
+    RF = RandomForest(X_train, X_test, depend_train, depend_test, 'no')
+    print("XGB best parameters for {0}: ", RF.best_param).format(criteria)
+    print("XGB prediction accuracy for {0}: ", RF.score).format(criteria)
 
     # Predicting dependent variable with Neural Network
     # NN = NeuralNetwork(X_train, X_test, depend_train, depend_test)
--- XGBoost.py
+++ XGBoost.py
@@ -2,37 +2,40 @@ from xgboost import XGBClassifier
 from hyperopt import hp, tpe, fmin, STATUS_OK, Trials
 from hyperopt.pyll import scope
 import numpy as np
+import pandas as pd
 
 
 class RandomForest:
-    def __init__(self, X_train, X_test, Y_train, Y_test):
-        # Initiate variables
-        train_x_fold_1, train_x_fold_2, train_x_fold_3, train_x_fold_4, train_x_fold_5 = [0 for _ in range(5)]
-        train_y_fold_1, train_y_fold_2, train_y_fold_3, train_y_fold_4, train_y_fold_5 = [0 for _ in range(5)]
-        val_x_fold_1, val_x_fold_2, val_x_fold_3, val_x_fold_4, val_x_fold_5 = [0 for _ in range(5)]
-        val_y_fold_1, val_y_fold_2, val_y_fold_3, val_y_fold_4, val_y_fold_5 = [0 for _ in range(5)]
+    def __init__(self, X_train, X_test, Y_train, Y_test, criteria):
 
         # Data preparation
-        myList = {''}
-        criteria = Y_train.dtype.names[0]
-        for i in range(1, 5):
-            myList.add('train_x_fold_{0}_{1}'.format(i, criteria))
-            myList.add('train_y_fold_{0}_{1}'.format(i, criteria))
-            myList.add('val_x_fold_{0}'.format(i))
-            myList.add('val_y_fold_{0}'.format(i))
+        myList = []
+        for i in range(1, 6):
+            myList.append('train_x_fold_{0}_{1}'.format(i, criteria))
+            myList.append('train_y_fold_{0}_{1}'.format(i, criteria))
+            myList.append('val_x_fold_{0}'.format(i))
+            myList.append('val_y_fold_{0}'.format(i))
 
-        gbl = globals()
+        i = 1
+        files = {}
         for toImport in myList:
-            i = 1
             file = "data/train_test_frames/" + toImport + ".csv"
-            if file.startswith('train_x'):
-                gbl['train_x_fold_{0}'.format(i)] = pd.read_csv(file).to_numpy()
-            else:
-                if file.startswith('train_y'):
-                    gbl['train_y_fold_{0}'.format(i)] = pd.read_csv(file).to_numpy()
-                    i = i+1
+            temp = pd.read_csv(file)
+            temp = temp.drop(columns={'Unnamed: 0'})
+            temp = temp.iloc[:, 1:]
+            if toImport.startswith('train'):
+                if toImport.startswith('train_x'):
+                    files['train_x_fold_{0}'.format(i)] = temp
                 else:
-                    gbl[file] = pd.read_csv(file).to_numpy()
+                    files['train_y_fold_{0}'.format(i)] = temp
+                    i = i + 1
+            else:
+                files[toImport] = temp
+
+        print(files.get('train_x_fold_1'))
+        print(files.get('train_y_fold_1'))
+        print(files.get('val_x_fold_1'))
+        print(files.get('val_y_fold_1'))
 
         # Hyperparameter sets
         hyperparams = {'n_estimators': scope.int(hp.quniform('n_estimators', 5, 35, 1)),
@@ -44,21 +47,21 @@ class RandomForest:
 
         def obj_func(params):
             clf = XGBClassifier(**params, use_label_encoder=False, objective="binary:logistic", eval_metric='error')
-            clf.fit(train_x_fold_1, train_y_fold_1)
-            pred_y_fold_1 = clf.predict(val_x_fold_1)
-            auc = roc_auc_score(val_y_fold_1, pred_y_fold_1)
-            clf.fit(train_x_fold_2, train_y_fold_2)
-            pred_y_fold_2 = clf.predict(val_x_fold_2)
-            auc = auc + roc_auc_score(val_y_fold_2, pred_y_fold_2)
-            clf.fit(train_x_fold_3, train_y_fold_3)
-            pred_y_fold_3 = clf.predict(val_x_fold_3)
-            auc = auc + roc_auc_score(val_y_fold_3, pred_y_fold_3)
-            clf.fit(train_x_fold_4, train_y_fold_4)
-            pred_y_fold_4 = clf.predict(val_x_fold_4)
-            auc = auc + roc_auc_score(val_y_fold_4, pred_y_fold_4)
-            clf.fit(train_x_fold_5, train_y_fold_5)
-            pred_y_fold_5 = clf.predict(val_x_fold_5)
-            auc = auc + roc_auc_score(val_y_fold_5, pred_y_fold_5)
+            clf.fit(files.get('train_x_fold_1'), files.get('train_y_fold_1'))
+            pred_y_fold_1 = clf.predict(files.get('val_x_fold_1'))
+            auc = roc_auc_score(files.get('val_y_fold_1'), pred_y_fold_1)
+            clf.fit(files.get('train_x_fold_2'), files.get('train_y_fold_2'))
+            pred_y_fold_2 = clf.predict(files.get('val_x_fold_2'))
+            auc = auc + roc_auc_score(files.get('val_y_fold_2'), pred_y_fold_2)
+            clf.fit(files.get('train_x_fold_3'), files.get('train_y_fold_3'))
+            pred_y_fold_3 = clf.predict(files.get('val_x_fold_3'))
+            auc = auc + roc_auc_score(files.get('val_y_fold_3'), pred_y_fold_3)
+            clf.fit(files.get('train_x_fold_4'), files.get('train_y_fold_4'))
+            pred_y_fold_4 = clf.predict(files.get('val_x_fold_4'))
+            auc = auc + roc_auc_score(files.get('val_y_fold_4'), pred_y_fold_4)
+            clf.fit(files.get('train_x_fold_5'), files.get('train_y_fold_5'))
+            pred_y_fold_5 = clf.predict(files.get('val_x_fold_5'))
+            auc = auc + roc_auc_score(files.get('val_y_fold_5'), pred_y_fold_5)
             return {'loss': -auc/5, 'status': STATUS_OK}
 
         trials = Trials()
@@ -66,9 +69,13 @@ class RandomForest:
                                rstate=np.random.RandomState(1))
         best_param_values = [x for x in self.best_param.values()]
 
-        RF_best = XGBClassifier(n_estimators=int(best_param_values[0]), learning_rate=best_param_values[1],
-                                subsample=best_param_values[2], max_depth=int(best_param_values[3]),
-                                colsample_bytree=best_param_values[4], min_child_weight=int(best_param_values[5]))
+        RF_best = XGBClassifier(n_estimators=best_param_values[0], learning_rate=best_param_values[1],
+                                subsample=best_param_values[2], max_depth=best_param_values[3],
+                                colsample_bytree=best_param_values[4], min_child_weight=best_param_values[5])
 
         RF_best.fit(X_train, Y_train)
+        prediction = RF_best.predict(X_test)
+        prediction = pd.DataFrame(prediction)
+        file_name = "data/prediction/prediction_{0}.csv".format(criteria)
+        prediction.to_csv(file_name)
         self.score = RF_best.score(X_test, Y_test)
