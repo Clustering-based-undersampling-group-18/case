81f0d189de7afe56888a22ed8d60d43cbd3a0b49
AnneJasmijnLangerak <annejasmijn@vanlangerak.com>
2021-03-09 12:43:58 +0100

    cleaning and comments

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -2,7 +2,7 @@
 This script contains functions that can be used to analyze the dataset
 (These are not used in a main file)
 """
-
+# Packages and modules
 import time
 import pandas as pd
 import numpy as np
@@ -10,6 +10,7 @@ from collections import Counter
 
 from networkx.drawing.tests.test_pylab import plt
 
+# Import the data
 frame = pd.read_csv("data/frame.csv",
                     dtype={'onTimeDelivery': str, 'datetTimeFirstDeliveryMoment': object, 'returnCode': object,
                            'transporterNameOther': object, 'cancellationReasonCode': object})
@@ -19,19 +20,19 @@ frame = pd.read_csv("data/frame.csv",
 def find_range(f):
     for column in f.keys():
         col = f[column]
-        print("Characteristic name: " + column)
+        print("Characteristic name: " + column)  # prints out which column is being analyzed
         print("Total column length: " + str(len(col)))
 
-        col.dropna(inplace=True)
+        col.dropna(inplace=True)  # Remove nan's/ None's
 
-        print("Number of items(None removed): " + str(len(col)))
+        print("Number of items(None removed): " + str(
+            len(col)))  # prints out how many actual observations the column has
         print("Number of unique items in column: " + str(col.nunique()))
         if col.nunique() < 50:
             print("Set of items: " + str(col.unique()))
         try:
             col = np.array(col)
-            # col_without_unknown = np.delete(col, np.where(col == "Unknown"))
-            col_without_unknown = col[col != "Unknown"]
+            col_without_unknown = col[col != "Unknown"]  # Remove the Unknown observations if present
             col_without_unknown = col_without_unknown.astype("float64")
             print("Minimum value (None removed): " + str(np.min(col_without_unknown)))
             print("Maximum value (None removed): " + str(np.max(col_without_unknown)))
@@ -44,79 +45,37 @@ def find_range(f):
         print("\n")
 
 
-# Function that is the slow version of the find_range function
-def check_unique_values(f):
-    unique_to_dict = {}
-    for column in f.keys():
-        if column == 'cntDistinctCaseIds' or column == 'transporterNameOther':
-            continue
-        temp_set = set(f[column])
-        unique_to_dict[column] = temp_set
-        print("Characteristic name: " + column)
-        print("Number of unique items: " + str(len(temp_set)))
-        if len(temp_set) < 25:
-            print("Items in set: " + str(temp_set))
-        try:
-            print("Minimum value: " + str(min(temp_set)))
-            print("Maximum value: " + str(max(temp_set)))
-            try:
-                print("Median value: " + str(np.median(np.array(f[column]))))
-            except:
-                pass
-        except:
-            find_range(f[column])
-        print("\n")
-        time.sleep(20)
-
-
 # Function that counts the occurrences of all values in a given column
 def count_classes(column_name, f):
     col = f[column_name]
     return col.value_counts(ascending=True)
 
 
-# Function that counts the classes belong to a certain column given that the order is returned, also ratio to no returns
-def most_returns(f, col_name):
-    # First find product group with most returns
-    returns = f[[col_name, 'noReturn']]
-    no_returns = returns[returns['noReturn'] == True]
-    returns = returns[returns['noReturn'] == False]
-    returns_sorted = count_classes(col_name, returns)
-    no_returns_sorted = count_classes(col_name, no_returns)
-    relative = returns_sorted.divide(no_returns_sorted, fill_value=None).dropna().sort_values()
-    return returns_sorted, relative
+# This function groups a frame by specified columns
+def groupby(frame, column1, column2):
+    counts = frame.groupby([column1, column2]).size()
+    return counts
 
 
-# Function that counts classes belonging to a certain column given that the order is cancelled,
-# also calculates ratio to no cancels
-def most_cancellation(f, col_name):
-    # First find product group with most cancellations
-    cancelled = f[[col_name, 'noCancellation']]
-    not_cancelled = cancelled[cancelled['noCancellation'] == True]
-    cancelled = cancelled[cancelled['noCancellation'] == False]
-    cancels_sorted = count_classes(col_name, cancelled)
-    no_cancels_sorted = count_classes(col_name, not_cancelled)
-    relative_cancels = cancels_sorted.divide(no_cancels_sorted, fill_value=None).dropna().sort_values()
-    return cancels_sorted, relative_cancels
+# Function that counts classes belonging to a certain column given that the order has not met a criterion,
+# also computes ratio
+def most_criteria(f, col_name, criterion):
+    not_met = f[[col_name, criterion]]
 
+    met = not_met[not_met[criterion] is True]
+    not_met = not_met[not_met[criterion] is False]
 
-# Function that counts classes belonging to a certain column given that the order has a case,
-# also calculates ratio to no cases
-def most_cases(f, col_name):
-    # first find product group with most cases
-    cases = f[[col_name, 'noCase']]
-    no_cases = cases[cases['noCase'] == True]
-    cases = cases[cases['noCase'] == False]
-    cases_sorted = count_classes(col_name, cases)
-    no_cases_sorted = count_classes(col_name, no_cases)
-    relative_cases = cases_sorted.divide(no_cases_sorted, fill_value=None).dropna().sort_values()
-    return cases_sorted, relative_cases
+    not_met_sorted = count_classes(col_name, not_met)
+    met_sorted = count_classes(col_name, met)
+
+    relative = not_met_sorted.divide(met_sorted, fill_value=None).dropna().sort_values()
+
+    return not_met_sorted, relative
 
 
 # Function that counts classes belonging to a certain column given that the order is not delivered on time,
 # also calculates ratio to no on time deliveries
 def least_on_time_delivery(f, col_name):
-    # first find product group with least on time deliveries
     late = f[[col_name, 'onTimeDelivery']]
     on_time = late[late['onTimeDelivery'] == "True"]
     cases = late[late['onTimeDelivery'] == "False"]
@@ -125,6 +84,26 @@ def least_on_time_delivery(f, col_name):
     on_time_sorted = count_classes(col_name, on_time)
     relative_late_deliveries = late_sorted.divide(on_time_sorted, fill_value=None).dropna().sort_values()
     return late_sorted, relative_late_deliveries
+
+
+# Function that calculates the class ratios
+def class_ratios(f):
+    total_orders = len(f)
+
+    return_amount = len(f[f["noReturn"] is False])
+    return_ratio = np.divide(total_orders, return_amount)
+
+    cancellation_amount = len(f[f["noCancellation"] is False])
+    cancellation_ratio = np.divide(total_orders, cancellation_amount)
+
+    # Note that this ratio is late to ontime+unknowns
+    late_delivery_amount = len(f[f["onTimeDelivery"] == "false"])
+    late_delivery_ratio = np.divide(total_orders, late_delivery_amount)
+
+    case_amount = len(f[f["noCase"] is False])
+    case_ratio = np.divide(total_orders, case_amount)
+
+    return return_ratio, cancellation_ratio, late_delivery_ratio, case_ratio
 
 
 # column can choose from cancellationDate [11], datetTimeFirstDeliveryMoment
@@ -141,6 +120,7 @@ def column_and_order_dates(data, column):
     return columndates, orderdates
 
 
+# This function transforms a given column containing dates into a days difference column
 def days_difference(columndates, orderdates, column_days):
     difference_dates = (columndates - orderdates) / np.timedelta64(1, 'D')
     difference_dates = difference_dates[difference_dates > -1]
@@ -154,6 +134,7 @@ def days_difference(columndates, orderdates, column_days):
     return difference_dates, date_values
 
 
+# This function computes the Pearson correlation of the criteria
 def correlations_categorical(data):
     main_variables = data[["noCancellation", "onTimeDelivery", "noCase", "noReturn"]]
     factorized_vars = main_variables.apply(lambda x: pd.factorize(x)[0])
@@ -161,13 +142,9 @@ def correlations_categorical(data):
     return correlation
 
 
-def groupby(frame, column1, column2):
-    counts = frame.groupby([column1, column2]).size()
-    return counts
-
-
-def thijs_function():
-    [columndates, orderdates] = column_and_order_dates(frame, "returnDateTime")
+# This function plots a histogram given a column
+def get_hist(frame, col):
+    [columndates, orderdates] = column_and_order_dates(frame, col)
     [difference_dates, date_values] = days_difference(columndates, orderdates, 31)
 
     plt.hist(x=difference_dates, bins=100)
@@ -176,16 +153,10 @@ def thijs_function():
     plt.show()
     print(date_values)
 
-    correlation = correlations_categorial(frame)
-    counts = groupby(frame, "noCase", "noCancellation")
-    counts1 = groupby(frame, "noCase", "onTimeDelivery")
-    counts2 = groupby(frame, "noCase", "noReturn")
-    counts3 = groupby(frame, "noCancellation", "onTimeDelivery")
-    counts4 = groupby(frame, "noCancellation", "noReturn")
-    counts5 = groupby(frame, "onTimeDelivery", "noReturn")
 
-
-def rest_code(frame):
+# With this function we can compute how many orders are caused by not meeting one or more criteria
+# Note that we utilized this function by manually changing criteria and their status
+def matches_causes_count(frame):
     print(frame[frame["detailedMatchClassification"] == "KNOWN HAPPY"])
     print(frame["returnDateTime"].unique())
     print(frame[(frame['detailedMatchClassification'] == "UNKNOWN") & (frame['noReturn'] == True) & (
@@ -197,28 +168,8 @@ def rest_code(frame):
     print(frame['detailedMatchClassification'].unique())
     print(frame[(frame['returnDateTime'].astype(float) < 30)])
     print(frame[(frame['returnDateTime'].astype(float) > 30) & (frame['onTimeDelivery'] == 'True') & (
-            frame['noCase'] == True) & (frame['noCancellation'] == True)])  # 105076
-    # print(frame.loc[807032, :])
+            frame['noCase'] == True) & (frame['noCancellation'] == True)])
 
     print("*" * 25)
-
-
-# Function that calculates the class ratios
-def class_ratios(frame):
-    total_orders = len(frame)
-
-    return_amount = len(frame[frame["noReturn"] == False])
-    return_ratio = np.divide(total_orders, return_amount)
-
-    cancellation_amount = len(frame[frame["noCancellation"] == False])
-    cancellation_ratio = np.divide(total_orders, cancellation_amount)
-
-    late_delivery_amount = len(frame[frame["onTimeDelivery"] == "false"])
-    late_delivery_ratio = np.divide(total_orders, late_delivery_amount)
-
-    case_amount = len(frame[frame["noCase"] == False])
-    case_ratio = np.divide(total_orders, case_amount)
-
-    return return_ratio, cancellation_ratio, late_delivery_ratio, case_ratio
 
 
